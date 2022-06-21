6da66a94869e82de137868c92a7b0fa52792da88
Divye Gupta <divyegupta00@gmail.com>
2022-06-21 17:50:05 +0530

    add: UI fixes for chrome

--- src/MyCV.js
+++ src/MyCV.js
@@ -4,7 +4,7 @@ import './css/MyCV.css'
 const MyCV = () => {
     return (
         <div className = "mycvmain" >
-            <iframe title='CV' src="https://drive.google.com/file/d/1afPfe1azuyBJ26-BctsJfNv0pU-h-gVW/preview" width="640" height="780" allow="autoplay"></iframe>   
+            <iframe title = 'CV' src="https://drive.google.com/file/d/1afPfe1azuyBJ26-BctsJfNv0pU-h-gVW/preview" width="640" height="850" allow="autoplay"></iframe>  
             <a href='https://drive.google.com/file/d/1afPfe1azuyBJ26-BctsJfNv0pU-h-gVW/view'><button>Download CV</button></a> 
         </div>
     )
